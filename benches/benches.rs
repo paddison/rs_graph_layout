7aa163db0ae50e192bf27ce5382be2640e9f3a4d
paddison <patrickbaumann579@gmail.com>
2024-05-10 14:23:52 +0200

    fix compilation errors

--- benches/benches.rs
+++ benches/benches.rs
@@ -10,8 +10,8 @@ pub fn benchmark_test(c: &mut Criterion) {
     let edges = LayeredGraphGenerator::new(10)
         .with_seed(123456)
         .with_degree(2)
-        .add_random_edges(6)
-        .build().iter().map(|(t, h)| (*t as u32, *h as u32)).collect::<Vec<(u32, u32)>>();
+        .add_random_edges(5)
+        .build().iter().map(|(t, h)| (*t as u32 + 1, *h as u32 + 1)).collect::<Vec<(u32, u32)>>();
 
     let vertices = edges.iter().flat_map(|(t, h)| vec![*t, *h]).collect::<HashSet<u32>>().into_iter().collect::<Vec<u32>>();
     c.bench_function("original 10", |b| b.iter(|| create_layouts_original(vertices.clone(), edges.clone(), 40, false)));
